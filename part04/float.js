5a0ed900c192f5514b23b61aded2026b8c4117ee
Scytar <scytarnoob@gmail.com>
2022-06-07 19:06:49 -0300

    Create float's closest integers page js part 04

---
+++ part04/float.js
@@ -0,0 +1,19 @@
+const userInput = document.querySelector("#userFloat");
+const findButton = document.querySelector("#findButton");
+const resultElement = document.querySelector("#result");
+
+findButton.addEventListener("click", findIntegers);
+
+function findIntegers() {
+    const userFloat = parseFloat(userInput.value);
+    const lowerInteger = Math.floor(userFloat);
+    const upperInteger = Math.ceil(userFloat);
+    let message = `The closest integers are ${lowerInteger} and ${upperInteger}.`;
+
+    if (isNaN(userFloat)) {
+        message = "Please enter a non integer number.";
+    }
+    resultElement.style.opacity = 1;
+    resultElement.innerHTML = message;
+    return
+}
