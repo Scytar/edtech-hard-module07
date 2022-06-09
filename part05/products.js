77189a53a645f481d004d739da69abad234aba81
Scytar <scytarnoob@gmail.com>
2022-06-09 11:05:57 -0300

    Add comment with instructions to add a button in products page

--- part05/products.js
+++ part05/products.js
@@ -1,6 +1,12 @@
 const productSelect = document.querySelector("#productSelect");
 const productDisplay = document.querySelector("#productDisplay");
 
+//Adding a button increments the number of user interactions, so it's not a good practice for a simple task such as show an product image
+//In order to add a button, the following code should be swapped with the code line 10, also, it's required to add an html button element in the HTML file between lines 30 and 31.
+//--Start of the code-- //
+//const viewButton = document.querySelector("#viewButton");
+//viewButton.addEventListener("click", showProduct);
+//--End of the code-- //
 productSelect.addEventListener("input", showProduct);
 
 function showProduct() {
