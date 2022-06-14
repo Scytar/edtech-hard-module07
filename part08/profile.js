c890c4f613d89fa17730d847ee932306b601c02f
Scytar <scytarnoob@gmail.com>
2022-06-13 21:17:52 -0300

    Add error messages and data verification for Profile JSON @ JS part 08

--- part08/profile.js
+++ part08/profile.js
@@ -15,31 +15,62 @@ const user = {
 };
 let userJSON = null;
 
-function saveProfile() {
-    if ((birthDateInput.value != "") && (genderInput != null)) {
-            
-        user.name = nameInput.value;
-        user.weight = parseFloat(weightInput.value);
-        user.height = parseInt(heightInput.value);
-        user.birthDate = birthDateInput.value;
-        user.gender = genderInput.value;
-        userJSON = JSON.stringify(user);
-        messageBox.textContent = "";
-    } else {
-        messageBox.textContent = "Please fill all fields"
-    }
-    console.log(userJSON)
-    return null;
-}
+let fieldIsInvalid = true;
+const resultDiv = document.querySelector("#resultDiv");
+const resultName = document.querySelector("#resultName");
+const resultWeight = document.querySelector("#resultWeight");
+const resultHeight = document.querySelector("#resultHeight");
+const resultBirthDate = document.querySelector("#resultBirthDate");
+const resultGender = document.querySelector("#resultGender");
+const resultJSON = document.querySelector("#resultJSON");
 
 function checkDate() {
     const birthDate = Date.parse(birthDateInput.value)
-    if (birthDate >= currentDate) {
-        messageBox.textContent = "Please enter a valid birth date!"
+    if ((birthDate >= currentDate) || (!birthDateInput.value)) {
+        return false;
     
-    } else{
-        messageBox.textContent = ""
+    } else {
+        return true;
     }
+    return null;
+}
+
+function saveProfile() {
+    messageBox.textContent = "";
+    
+    user.name = nameInput.value;
+    user.weight = parseFloat(weightInput.value);
+    user.height = parseInt(heightInput.value);
+    user.birthDate = birthDateInput.value;
+    user.gender = genderInput.value;
+    userJSON = JSON.stringify(user);
+    
+    try {
+        fieldIsInvalid = false;
+        if (nameInput.value.length < 5) throw 'Field "Name" is invalid!';
+        if (isNaN(weightInput.value) || weightInput.value === "") throw 'Field "Weight" is invalid!';
+        if (isNaN(heightInput.value) || heightInput.value === "") throw 'Field "Height" is invalid!';
+        if (!checkDate()) throw 'Field "Birth Date" is invalid!';
+        if (genderInput.value === "null") throw 'Field "Gender" is invalid!';
+    }
+
+    catch(err) {
+        messageBox.textContent = err;
+        fieldIsInvalid = true;
+    }
+
+    if (!fieldIsInvalid) {
+        resultDiv.style.display = "block";
+        resultName.textContent = user.name;
+        resultWeight.textContent = user.weight;
+        resultHeight.textContent = user.height;
+        resultBirthDate.textContent = user.birthDate;
+        resultGender.textContent = user.gender;
+        resultJSON.textContent = userJSON;
+
+        console.log(fieldIsInvalid,userJSON);
+    }
+    
     return null;
 }
 
