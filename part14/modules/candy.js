dd7712efe95d8e7e5107e1557d3ba8791059160c
Scytar <scytarnoob@gmail.com>
2022-07-01 15:41:11 -0300

    Fix <img> src link for github gitpages @ JS Part 14

--- part14/modules/candy.js
+++ part14/modules/candy.js
@@ -3,7 +3,7 @@ export function renderCandy() {
 
     app.innerHTML = `
     <div id="candy" class="contentDiv">
-        <figure><img src="../edtech-hard-module07/assets/imgs/candies.jpg" alt=""></figure>
+        <figure><img src="../edtech-hard-module07/part14/assets/imgs/candies.jpg" alt=""></figure>
         <h1>A Lot of Candies</h1>
         <h3>Dive into a olympic pool of sweetness!</h3>
         <p>Come discover a whole new world of flavours</p>
