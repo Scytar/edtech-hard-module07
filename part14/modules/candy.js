e31a8bbd242ef9f686ea168f6087c242cd29f7f9
Scytar <scytarnoob@gmail.com>
2022-07-01 13:37:58 -0300

    Change <img> src @ JS Part 14

--- part14/modules/candy.js
+++ part14/modules/candy.js
@@ -3,7 +3,7 @@ export function renderCandy() {
 
     app.innerHTML = `
     <div id="candy" class="contentDiv">
-        <figure><img src="../assets/imgs/candies.jpg" alt=""></figure>
+        <figure><img src="./assets/imgs/candies.jpg" alt=""></figure>
         <h1>A Lot of Candies</h1>
         <h3>Dive into a olympic pool of sweetness!</h3>
         <p>Come discover a whole new world of flavours</p>
--- part14/modules/home.js
+++ part14/modules/home.js
@@ -3,7 +3,7 @@ export function renderHome() {
 
     app.innerHTML = `
         <div id="home" class="contentDiv">
-            <figure class="logoWrapper"><img class="noShadow" src="../assets/imgs/logo.png" alt=""></figure>
+            <figure class="logoWrapper"><img class="noShadow" src="./assets/imgs/logo.png" alt=""></figure>
             <h1>Wendy's Candy</h1>
             <h3>World's best fictional candy store!</h3>
         </div>
