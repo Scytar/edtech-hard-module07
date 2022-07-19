bb4baba5145b0a77ea99cf921fb670f747966c26
Scytar <scytarnoob@gmail.com>
2022-07-19 15:21:57 -0300

    Add custom promise to weather API fetch

--- part17/index.js
+++ part17/index.js
@@ -48,12 +48,23 @@ function locationFetch(UF) {
 };
 
 function weatherFetch(id) {
-    
-    //Fetch from IBGE's Weather Prediction API
-    fetch(`https://apiprevmet3.inmet.gov.br/previsao/${id}`)
-    .then(res => {
-        res.json().then(data =>{
-            
+
+    //Create Custom Promise
+    return new Promise((resolve, reject) => {
+
+        //Fetch from IBGE's Weather Prediction API
+        fetch(`https://apiprevmet3.inmet.gov.br/previsao/${id}`)
+        .then(res => {
+
+            //Return fetched JSON if status is OK
+            if (res.status === 200) {
+                return res.json();
+            };
+            //Return error if status is not OK
+            return reject('Failed to fetch data')
+        })
+        .then(data =>{
+
             //Today + next 3 days
             const day1 = new Date();
             const day2 = new Date(Date.parse(day1) + 86400000);
@@ -66,88 +77,153 @@ function weatherFetch(id) {
             const day3DMY = `${(day3.getDate()).toString().padStart(2,'0')}/${(day3.getMonth()+1).toString().padStart(2,'0')}/${day3.getFullYear()}`;
             const day4DMY = `${(day4.getDate()).toString().padStart(2,'0')}/${(day4.getMonth()+1).toString().padStart(2,'0')}/${day4.getFullYear()}`;
 
-            const weekday = ["Doming","Segunda","Terça","Quarta","Quinta","Sexta","Sábado"];
-
-            // console.log(day1);
-            // console.log(day2);
-            // console.log(day3);
-            // console.log(data[id]);
-            // console.log(data[id][`${(day1.getDate()).toString().padStart(2,'0')}/${(day1.getMonth()+1).toString().padStart(2,'0')}/${day1.getFullYear()}`])
-
-            //Reset result display
-            result.innerHTML = `
-                <div class="dataWrapper">
-                    <div class="showData dateLabel">
-                        <span>${weekday[day1.getDay()]}</span>
-                        <span>${day1DMY}</span>
-                    </div>
-                    <div class="showData">
-                        <img src=${data[id][day1DMY]['manha']['icone']}></img>
-                        <span><em>Manhã</em> - ${data[id][day1DMY]['manha']['temp_min']}°C ~ ${data[id][day1DMY]['manha']['temp_max']}°C</span>
-                        <span>${data[id][day1DMY]['manha']['resumo']}</span>
-                    </div>
-                    <div class="showData">
-                        <img src=${data[id][day1DMY]['tarde']['icone']}></img>
-                        <span><em>Tarde</em> - ${data[id][day1DMY]['tarde']['temp_min']}°C ~ ${data[id][day1DMY]['tarde']['temp_max']}°C</span>
-                        <span>${data[id][day1DMY]['tarde']['resumo']}</span>
-                    </div>
-                    <div class="showData">
-                        <img src=${data[id][day1DMY]['noite']['icone']}></img>
-                        <span><em>Noite</em> - ${data[id][day1DMY]['noite']['temp_min']}°C ~ ${data[id][day1DMY]['noite']['temp_max']}°C</span>
-                        <span>${data[id][day1DMY]['noite']['resumo']}</span>
-                    </div>
-                </div>
-                
-                <div class="dataWrapper">
-                    <div class="showData dateLabel">
-                    <span>${weekday[day2.getDay()]}</span>
-                    <span>${day2DMY}</span>
-                    </div>
-                    <div class="showData">
-                        <img src=${data[id][day2DMY]['manha']['icone']}></img>
-                        <span><em>Manhã</em> - ${data[id][day2DMY]['manha']['temp_min']}°C ~ ${data[id][day2DMY]['manha']['temp_max']}°C</span>
-                        <span>${data[id][day2DMY]['manha']['resumo']}</span>
-                    </div>
-                    <div class="showData">
-                        <img src=${data[id][day2DMY]['tarde']['icone']}></img>
-                        <span><em>Tarde</em> - ${data[id][day2DMY]['tarde']['temp_min']}°C ~ ${data[id][day2DMY]['tarde']['temp_max']}°C</span>
-                        <span>${data[id][day2DMY]['tarde']['resumo']}</span>
-                    </div>
-                    <div class="showData">
-                        <img src=${data[id][day2DMY]['noite']['icone']}></img>
-                        <span><em>Noite</em> - ${data[id][day2DMY]['noite']['temp_min']}°C ~ ${data[id][day2DMY]['noite']['temp_max']}°C</span>
-                        <span>${data[id][day2DMY]['noite']['resumo']}</span>
-                    </div>
-                </div>
-                
-                <div class="dataWrapper">
-                    <div class="showData dateLabel">
-                    <span>${weekday[day3.getDay()]}</span>
-                    <span>${day3DMY}</span>
-                    </div>
-                    <div class="showData">
-                        <img src=${data[id][day3DMY]['icone']}></img>
-                        <span>${data[id][day3DMY]['temp_min']}°C ~ ${data[id][day3DMY]['temp_max']}°C</span>
-                        <span>${data[id][day3DMY]['resumo']}</span>
-                    </div>
-                </div>
-                
-                <div class="dataWrapper">
-                    <div class="showData dateLabel">
-                    <span>${weekday[day4.getDay()]}</span>
-                    <span>${day4DMY}</span>
-                    </div>
-                    <div class="showData">
-                        <img src=${data[id][day4DMY]['icone']}></img>
-                        <span>${data[id][day4DMY]['temp_min']}°C ~ ${data[id][day4DMY]['temp_max']}°C</span>
-                        <span>${data[id][day4DMY]['resumo']}</span>
-                    </div>
-                </div>
-
-            `;
-
+            //Select data to be forwarded
+            const newData = [
+                [//First day
+                    {//Morning
+                        weekday: data[id][day1DMY]['manha']['dia_semana'],
+                        icon: data[id][day1DMY]['manha']['icone'],
+                        summary: data[id][day1DMY]['manha']['resumo'],
+                        tMax: data[id][day1DMY]['manha']['temp_max'],
+                        tMin: data[id][day1DMY]['manha']['temp_min'],
+                        date: day1DMY
+                    },
+                    {//Afternoon
+                        weekday: data[id][day1DMY]['manha']['dia_semana'],
+                        icon: data[id][day1DMY]['manha']['icone'],
+                        summary: data[id][day1DMY]['manha']['resumo'],
+                        tMax: data[id][day1DMY]['manha']['temp_max'],
+                        tMin: data[id][day1DMY]['manha']['temp_min']
+                    },
+                    {//Night
+                        weekday: data[id][day1DMY]['manha']['dia_semana'],
+                        icon: data[id][day1DMY]['manha']['icone'],
+                        summary: data[id][day1DMY]['manha']['resumo'],
+                        tMax: data[id][day1DMY]['manha']['temp_max'],
+                        tMin: data[id][day1DMY]['manha']['temp_min']
+                    }
+                ],
+                [//Second day
+                    {//Morning
+                        weekday: data[id][day2DMY]['manha']['dia_semana'],
+                        icon: data[id][day2DMY]['manha']['icone'],
+                        summary: data[id][day2DMY]['manha']['resumo'],
+                        tMax: data[id][day2DMY]['manha']['temp_max'],
+                        tMin: data[id][day2DMY]['manha']['temp_min'],
+                        date: day2DMY
+                    },
+                    {//Afternoon
+                        weekday: data[id][day2DMY]['manha']['dia_semana'],
+                        icon: data[id][day2DMY]['manha']['icone'],
+                        summary: data[id][day2DMY]['manha']['resumo'],
+                        tMax: data[id][day2DMY]['manha']['temp_max'],
+                        tMin: data[id][day2DMY]['manha']['temp_min']
+                    },
+                    {//Night
+                        weekday: data[id][day2DMY]['manha']['dia_semana'],
+                        icon: data[id][day2DMY]['manha']['icone'],
+                        summary: data[id][day2DMY]['manha']['resumo'],
+                        tMax: data[id][day2DMY]['manha']['temp_max'],
+                        tMin: data[id][day2DMY]['manha']['temp_min']
+                    }
+                ],
+                {//Third day
+                    weekday: data[id][day3DMY]['dia_semana'],
+                    icon: data[id][day3DMY]['icone'],
+                    summary: data[id][day3DMY]['resumo'],
+                    tMax: data[id][day3DMY]['temp_max'],
+                    tMin: data[id][day3DMY]['temp_min'],
+                    date: day3DMY
+                },
+                {//Fourth day
+                    weekday: data[id][day4DMY]['dia_semana'],
+                    icon: data[id][day4DMY]['icone'],
+                    summary: data[id][day4DMY]['resumo'],
+                    tMax: data[id][day4DMY]['temp_max'],
+                    tMin: data[id][day4DMY]['temp_min'],
+                    date: day4DMY
+                }
+            ]
+            resolve(renderPrediction(newData))
         })
+        .catch(error => {reject(error)})
     })
+}
+
+function renderPrediction(data) {
+
+    //Reset result display and render
+    result.innerHTML = `
+        <div class="dataWrapper">
+            <div class="showData dateLabel">
+                <span>${data[0][0]['weekday']}</span>
+                <span>${data[0][0]['date']}</span>
+            </div>
+            <div class="showData">
+                <img src=${data[0][0]['icon']}></img>
+                <span><em>Manhã</em> - ${data[0][0]['tMin']}°C ~ ${data[0][0]['tMax']}°C</span>
+                <span>${data[0][0]['summary']}</span>
+            </div>
+            <div class="showData">
+                <img src=${data[0][1]['icon']}></img>
+                <span><em>Manhã</em> - ${data[0][1]['tMin']}°C ~ ${data[0][1]['tMax']}°C</span>
+                <span>${data[0][1]['summary']}</span>
+            </div>
+            <div class="showData">
+                <img src=${data[0][2]['icon']}></img>
+                <span><em>Manhã</em> - ${data[0][2]['tMin']}°C ~ ${data[0][2]['tMax']}°C</span>
+                <span>${data[0][2]['summary']}</span>
+            </div>
+        </div>
+        
+        <div class="dataWrapper">
+            <div class="showData dateLabel">
+                <span>${data[1][0]['weekday']}</span>
+                <span>${data[1][0]['date']}</span>
+            </div>
+            <div class="showData">
+                <img src=${data[1][0]['icon']}></img>
+                <span><em>Manhã</em> - ${data[1][0]['tMin']}°C ~ ${data[1][0]['tMax']}°C</span>
+                <span>${data[1][0]['summary']}</span>
+            </div>
+            <div class="showData">
+                <img src=${data[1][1]['icon']}></img>
+                <span><em>Manhã</em> - ${data[1][1]['tMin']}°C ~ ${data[1][1]['tMax']}°C</span>
+                <span>${data[1][1]['summary']}</span>
+            </div>
+            <div class="showData">
+                <img src=${data[1][2]['icon']}></img>
+                <span><em>Manhã</em> - ${data[1][2]['tMin']}°C ~ ${data[1][2]['tMax']}°C</span>
+                <span>${data[1][2]['summary']}</span>
+            </div>
+        </div>
+        
+        <div class="dataWrapper">
+            <div class="showData dateLabel">
+                <span>${data[2]['weekday']}</span>
+                <span>${data[2]['date']}</span>
+            </div>
+            <div class="showData">
+                <img src=${data[2]['icon']}></img>
+                <span>${data[2]['tMin']}°C ~ ${data[2]['tMax']}°C</span>
+                <span>${data[2]['summary']}</span>
+            </div>
+        </div>
+        
+        <div class="dataWrapper">
+            <div class="showData dateLabel">
+                <span>${data[3]['weekday']}</span>
+                <span>${data[3]['date']}</span>
+            </div>
+            <div class="showData">
+                <img src=${data[3]['icon']}></img>
+                <span>${data[3]['tMin']}°C ~ ${data[3]['tMax']}°C</span>
+                <span>${data[3]['summary']}</span>
+            </div>
+        </div>
+        </div>
+
+    `;
 }
 
 selectUF.addEventListener('change', (e) => {locationFetch(e.target.value)});
